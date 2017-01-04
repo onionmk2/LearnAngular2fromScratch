7c00b19a942412d5476df74c17389c317fcd5983
angular-cli <angular-cli@angular.io>
2017-01-04 12:23:10 +0900

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { LearnAngular2fromScratchPage } from './app.po';
+
+describe('learn-angular2from-scratch App', function() {
+  let page: LearnAngular2fromScratchPage;
+
+  beforeEach(() => {
+    page = new LearnAngular2fromScratchPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
